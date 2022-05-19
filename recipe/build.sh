104583ca9c8372e880699b67fb54f9068a2a1c63
duncan.macleod <duncan.macleod@ligo.org>
2022-05-19 10:35:24 +0100

    move activates to files and improve env variables
    includes two patches from upstream that will be included in 3.2.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,22 +20,11 @@ cmake --build . --parallel ${CPU_COUNT} --verbose --target install
 
 # -- build activate script
 
-ACTIVATE_SH="${PREFIX}/etc/conda/activate.d/activate_${PKG_NAME}.sh"
-mkdir -p $(dirname ${ACTIVATE_SH})
-cat > ${ACTIVATE_SH} << EOF
-#!/bin/bash
-export CONDA_BACKUP_GWOLLUM_DATA="\${GWOLLUM_DATA:-empty}"
-export GWOLLUM_DATA="/opt/anaconda1anaconda2anaconda3/share/data"
-EOF
-# deactivate.sh
-DEACTIVATE_SH="${PREFIX}/etc/conda/deactivate.d/deactivate_${PKG_NAME}.sh"
-mkdir -p $(dirname ${DEACTIVATE_SH})
-cat > ${DEACTIVATE_SH} << EOF
-#!/bin/bash
-if [ "\${CONDA_BACKUP_GWOLLUM_DATA}" = "empty" ]; then
-	unset GWOLLUM_DATA
-else
-	export GWOLLUM_DATA="\${CONDA_BACKUP_GWOLLUM_DATA}"
-fi
-unset CONDA_BACKUP_GWOLLUM_DATA
-EOF
+for action in activate deactivate; do
+	mkdir -p ${PREFIX}/etc/conda/${action}.d
+	for ext in sh csh; do
+		_target="${PREFIX}/etc/conda/${action}.d/activate-${PKG_NAME}.${ext}"
+		echo "-- Installing: ${_target}"
+		cp "${RECIPE_DIR}/${action}.${ext}" "${_target}"
+	done
+done
